a1168949d54bdf3d2efd197995c518400b7dc8fd
LPSJ1 <victormwakisagu@gmail.com>
2025-06-03 13:48:08 +0300

    commit 1
    This displays application of Liskov principle

---
+++ Money.java
@@ -0,0 +1,18 @@
+public abstract class Money 
+{
+    int amount;
+    char currency;
+
+    public abstract void convert_currency();
+}
+
+class Payment extends Money{
+
+public void convert_currency() {
+        
+        System.out.println("Converting currency for payment.");
+    }
+}
+
+//The class payment extends from money without adding onto any functionality.
+// It simply implements the abstract method convert_currency.
