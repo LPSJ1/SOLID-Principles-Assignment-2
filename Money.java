59b47b762e1b9eabcf472721efa2f96d6bf07b3e
LPSJ1 <victormwakisagu@gmail.com>
2025-06-03 13:55:30 +0300

    without liskov change

--- Money.java
+++ Money.java
@@ -1,16 +1,38 @@
-//Liskov principle
-public abstract class Money 
-{
+//Without liskov principle
+
+
+public abstract class Money  {
     int amount;
     char currency;
 
     public abstract void convert_currency();
 }
 
-class Payment extends Money{
-
-public void convert_currency() {
+class Payment extends Money {
+    @Override
+    public void convert_currency() {
+        // this violates Liskov because its Changing expected behavior by adding onto it
         
+    }
+}
+
+
+
+
+
+//Liskov principle
+// Correctly following Liskov Substitution Principle
+public abstract class Money  {
+    int amount;
+    char currency;
+
+    public abstract void convert_currency();
+}
+
+class Payment extends Money {
+    @Override
+    public void convert_currency() {
+        // Following Liskov: Implements expected behavior
         System.out.println("Converting currency for payment.");
     }
 }
